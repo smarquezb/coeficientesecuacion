3abe1c96f7741f6bbf53de059613eefb1c5ee4f6
Santiago Marquez betancur <santimb70@gmail.com>
2024-09-13 21:47:14 -0500

    proceso

--- coeficientesecuacion/Program.cs
+++ coeficientesecuacion/Program.cs
@@ -3,12 +3,23 @@
     static void Main(string[]args)
     {
         // ingresar datos
-
         Console.Write("Ingrese el coeficiente a: ");
         double a = Convert.ToDouble(Console.ReadLine());
 
         Console.Write("Ingrese el coeficiente b: ");
         double b = Convert.ToDouble(Console.ReadLine());
 
-     
-
+        // Verificamos si a es cero
+        if (a == 0)
+        {
+            // Si a es cero, la ecuación no tiene solución o todos los números son solución
+            if (b == 0)
+            {
+                Console.WriteLine("Todos los números son solución");
+            }
+            else
+            {
+                Console.WriteLine("La ecuación no tiene solución");
+            }
+        }
+        
