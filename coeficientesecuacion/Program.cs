976cab7632a541747577ab3d2f14eedaeda8a87d
Santiago Marquez betancur <santimb70@gmail.com>
2024-09-13 21:40:36 -0500

    Add project files.

---
+++ coeficientesecuacion/Program.cs
@@ -0,0 +1,14 @@
+﻿public class ecuacion
+{
+    static void Main(string[]args)
+    {
+        // ingresar datos
+
+        Console.Write("Ingrese el coeficiente a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese el coeficiente b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+     
+
